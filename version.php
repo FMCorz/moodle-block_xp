d388b16daed68b48fb9b72979a520d76bfb11b20
Frederic Massart <fmcell@gmail.com>
2015-03-13 13:24:46 +0100

    Bump to version 1.5

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->version    = 2015031300;
 $plugin->requires   = 2014041500;
 $plugin->component  = 'block_xp';
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '1.5-dev';
+$plugin->release    = '1.5';
 $plugin->cron       = 84600;
