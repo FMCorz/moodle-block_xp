abcc988377d309d143f121963cdc9325ec7f23e3
Frédéric Massart <fred@branchup.tech>
2022-02-11 17:40:06 +0800

    Possible fatal error when throwing coding_exception

--- classes/local/xp/admin_filter_manager.php
+++ classes/local/xp/admin_filter_manager.php
@@ -182,7 +182,7 @@ class admin_filter_manager {
     public function reset_all_courses_to_defaults() {
         $config = di::get('config');
         if ($config->get('context') != CONTEXT_COURSE) {
-            throw coding_exception('Cannot reset filters for all courses in current mode.');
+            throw new coding_exception('Cannot reset filters for all courses in current mode.');
         }
         $courseworldfactory = di::get('course_world_factory');
 
