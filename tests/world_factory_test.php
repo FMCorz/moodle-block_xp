385c9768b9020ea3fbd001947ce2fe4f9d58dacd
Frédéric Massart <fred@branchup.tech>
2024-04-22 09:39:17 +0800

    Unit test to confirm behaviour of context world factory in system mode

--- tests/world_factory_test.php
+++ tests/world_factory_test.php
@@ -25,7 +25,10 @@
 
 namespace block_xp;
 
+use block_xp\local\course_world;
 use block_xp\tests\base_testcase;
+use context_course;
+use context_system;
 
 /**
  * Test case.
@@ -55,15 +58,15 @@ final class world_factory_test extends base_testcase {
 
         $w1 = $factory->get_world($c1->id);
         $this->assertEquals($w1->get_courseid(), $c1->id);
-        $this->assertEquals($w1->get_context()->id, \context_course::instance($c1->id)->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w1);
+        $this->assertEquals($w1->get_context()->id, context_course::instance($c1->id)->id);
+        $this->assertInstanceOf(course_world::class, $w1);
         $w1b = $factory->get_world((string) $c1->id);
         $this->assertSame($w1, $w1b);
 
         $w2 = $factory->get_world($c2->id);
         $this->assertEquals($w2->get_courseid(), $c2->id);
-        $this->assertEquals($w2->get_context()->id, \context_course::instance($c2->id)->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w2);
+        $this->assertEquals($w2->get_context()->id, context_course::instance($c2->id)->id);
+        $this->assertInstanceOf(course_world::class, $w2);
 
         $w2b = $factory->get_world((string) $c2->id);
         $this->assertSame($w2, $w2b);
@@ -93,8 +96,8 @@ final class world_factory_test extends base_testcase {
         $w2 = $factory->get_world($c2->id);
 
         $this->assertEquals($w0->get_courseid(), SITEID);
-        $this->assertEquals($w0->get_context()->id, \context_system::instance()->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w1);
+        $this->assertEquals($w0->get_context()->id, context_system::instance()->id);
+        $this->assertInstanceOf(course_world::class, $w0);
         $this->assertSame($w0, $w1);
         $this->assertSame($w1, $w2);
     }
@@ -109,8 +112,8 @@ final class world_factory_test extends base_testcase {
         $c1 = $this->getDataGenerator()->create_course();
         $c2 = $this->getDataGenerator()->create_course();
 
-        $ctx1 = \context_course::instance($c1->id);
-        $ctx2 = \context_course::instance($c2->id);
+        $ctx1 = context_course::instance($c1->id);
+        $ctx2 = context_course::instance($c2->id);
 
         $factory = di::get('context_world_factory');
         $config = di::get('config');
@@ -121,25 +124,54 @@ final class world_factory_test extends base_testcase {
         $w1 = $factory->get_world_from_context($ctx1);
         $this->assertEquals($w1->get_courseid(), $c1->id);
         $this->assertEquals($w1->get_context()->id, $ctx1->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w1);
+        $this->assertInstanceOf(course_world::class, $w1);
         $w1b = $factory->get_world_from_context($ctx1);
         $this->assertSame($w1, $w1b);
 
         $w2 = $factory->get_world_from_context($ctx2);
         $this->assertEquals($w2->get_courseid(), $c2->id);
         $this->assertEquals($w2->get_context()->id, $ctx2->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w2);
+        $this->assertInstanceOf(course_world::class, $w2);
 
         $this->assertNotEquals($w1, $w2);
         $this->assertNotEquals($w1->get_courseid(), $w2->get_courseid());
         $this->assertNotEquals($w1->get_context()->id, $w2->get_context()->id);
 
         // System context behaviour to return "frontpage".
-        $w0 = $factory->get_world_from_context(\context_system::instance());
+        $w0 = $factory->get_world_from_context(context_system::instance());
         $this->assertEquals($w0->get_courseid(), SITEID);
-        $this->assertEquals($w0->get_context()->id, \context_system::instance()->id);
-        $this->assertInstanceOf(\block_xp\local\course_world::class, $w0);
+        $this->assertEquals($w0->get_context()->id, context_system::instance()->id);
+        $this->assertInstanceOf(course_world::class, $w0);
         $this->assertNotEquals($w0, $w1);
+    }
+
+    /**
+     * Test context world factory in system.
+     *
+     * @covers \block_xp\local\factory\context_world_factory
+     * @covers \block_xp\local\factory\default_context_world_factory
+     */
+    public function test_context_world_factory_in_system(): void {
+        $c1 = $this->getDataGenerator()->create_course();
+        $c2 = $this->getDataGenerator()->create_course();
+
+        $ctx1 = context_course::instance($c1->id);
+        $ctx2 = context_course::instance($c2->id);
+
+        $config = di::get('config');
+        $config->set('context', CONTEXT_SYSTEM);
+        $factory = di::get('context_world_factory');
+
+        // Confirm that the context does not really matter.
+        $w0 = $factory->get_world_from_context(context_system::instance());
+        $w1 = $factory->get_world_from_context($ctx1);
+        $w2 = $factory->get_world_from_context($ctx2);
+
+        $this->assertEquals($w0->get_courseid(), SITEID);
+        $this->assertEquals($w0->get_context()->id, context_system::instance()->id);
+        $this->assertInstanceOf(course_world::class, $w0);
+        $this->assertSame($w0, $w1);
+        $this->assertSame($w1, $w2);
     }
 
 }
