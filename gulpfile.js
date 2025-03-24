c56c69325190247c1b026c39a6d89c3562806cf8
Frédéric Massart <fred@branchup.tech>
2025-03-24 13:35:25 +0800

    Build files no longer exclude their module name

--- gulpfile.js
+++ gulpfile.js
@@ -63,19 +63,7 @@ function moodleGruntAmd(cb) {
   });
 }
 
-function removeAmdDefinedName(cb) {
-  // We must do this to support older Moodle versions that did not expect
-  // the name to be included as part of the build file. That can be seen
-  // in Moodle 3.3, and maybe older versions, where the code will attempt
-  // to add the module even if it is already present. As of Moodle 4.0,
-  // the module name will be injected if not present, so we can safely remove it.
-  return gulp
-    .src('./amd/build/*.js')
-    .pipe(replace(/^\s*define\s*\(\s*['"][a-z0-9_/-]+['"]\s*,/m, 'define('))
-    .pipe(gulp.dest('./amd/build'));
-}
-
-const moodleAmd = gulp.series(moodleGruntAmd, removeAmdDefinedName);
+const moodleAmd = gulp.series(moodleGruntAmd);
 
 /** Webpack. */
 
