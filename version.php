9291ef443d6556e40379729077f207b7c79e12a3
Frédéric Massart <fred@branchup.tech>
2025-04-11 10:24:32 +0800

    Bump to 18.0-rc1

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2025032801;
+$plugin->version    = 2025041100;
 $plugin->requires   = 2022112800;   // Moodle 4.1.0.
 $plugin->component  = 'block_xp';
 $plugin->maturity   = MATURITY_ALPHA;
-$plugin->release    = '18.0-dev';
+$plugin->release    = '18.0-rc1';
 $plugin->supported  = [401, 500];
