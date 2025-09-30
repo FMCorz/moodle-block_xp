5fd993df00099f18b0a6a0c73f2acf588385c31f
Frédéric Massart <fred@branchup.tech>
2025-09-30 18:50:27 +0800

    Course selector remembers recently selected entries

--- amd/src/course-selector.js
+++ amd/src/course-selector.js
@@ -27,6 +27,7 @@ import Modal from 'core/modal';
 import ModalEvents from 'core/modal_events';
 import {asyncRender} from 'block_xp/compat';
 import CourseResourceSelector from 'block_xp/course-resource-selector';
+import Config from 'core/config';
 import $ from 'jquery';
 
 export const openCourseSelector = async(onSelected) => {
@@ -42,11 +43,65 @@ export const openCourseSelector = async(onSelected) => {
         const container = root.querySelector('.search-result-contents');
         const termField = root.querySelector('.search-term-course');
         const cs = new CourseResourceSelector($(container), $(termField));
+        const recent = getRecentlyUsed();
+        if (recent.length) {
+            cs.displayResults(recent.slice(0, 10));
+        }
         cs.onResourceSelected(function(e, resource) {
+            saveRecentlyUsed(resource);
             onSelected(resource.course);
             modal.hide();
         });
     });
 
     modal.show();
+};
+
+const getRecentlyUsedCacheKey = () => {
+    const userId = Config.userId;
+    if (!userId) {
+        return null;
+    }
+    return 'block_xp_course_selector_recents_' + userId;
+};
+
+const getRecentlyUsed = () => {
+    const key = getRecentlyUsedCacheKey();
+    if (!key) {
+        return [];
+    }
+
+    const data = readArrayFromStorage(key);
+    const threshold = Date.now() - 5 * 24 * 60 * 60 * 1000; // 2 days ago.
+    return data.filter(d => d.time > threshold).sort((a, b) => b.time - a.time).map(d => d.resource);
+};
+
+const readFromStorage = (key, fallback = null) => {
+    try {
+        const data = sessionStorage.getItem(key);
+        return data !== null ? JSON.parse(data) : fallback;
+    } catch (e) {
+        return fallback;
+    }
+};
+
+const readArrayFromStorage = (key) => {
+    const data = readFromStorage(key, []);
+    return Array.isArray(data) ? data : [];
+};
+
+const saveRecentlyUsed = (resource) => {
+    const key = getRecentlyUsedCacheKey();
+    if (!key) {
+        return;
+    }
+
+    let data = readArrayFromStorage(key);
+    data = data.filter(d => d.resource.course.id !== resource.course.id);
+    data.push({
+        time: Date.now(),
+        resource: resource,
+    });
+
+    sessionStorage.setItem(key, JSON.stringify(data));
 };
